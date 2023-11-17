18e4ff4328686b830142ce38398630e855401644
taehun-kim2 <taehun.kim2@stradvision.com>
2023-11-17 17:57:12 +0900

    [CDG-1176] add logging

--- train.sh
+++ train.sh
@@ -6,6 +6,7 @@ PORT=${PORT:-29500}
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+DT=$(date '+%d%m%Y_%H%M%S');
 
 NUM_GPUS=$(($(echo $GPUS | grep -o ',' | wc -l) + 1)) # count number of ',' and plus one
 
@@ -17,6 +18,8 @@ echo "PORT: $PORT"
 echo "MASTER_ADDR: $MASTER_ADDR"
 echo "NUM_GPUS: $NUM_GPUS"
 
+mkdir -p logs
+
 MKL_NUM_THREADS=4 \
 OMP_NUM_THREADS=4 \
 CUDA_VISIBLE_DEVICES=$GPUS \
@@ -27,4 +30,4 @@ torchrun \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$NUM_GPUS \
     --master_port=$PORT \
-    tools/train.py $CONFIG > logs/monodetr.log
+    tools/train.py $CONFIG > logs/monodetr-$DT.log
